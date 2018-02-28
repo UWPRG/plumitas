b8950662d7329f03e4cde33b264c51b7d6acc65b
Josh <jsmith07@uw.edu>
2018-02-28 15:51:54 -0800

    Add multi-COLVAR and unbiased support to . Update tests accordingly.

--- plumitas/plumitas.py
+++ plumitas/plumitas.py
@@ -7,7 +7,7 @@ __all__ = ["read_colvar", "read_hills", "make_2d_free_energy_surface",
            "potential_of_mean_force"]
 
 
-def read_colvar(filename):
+def read_colvar(filename='COLVAR', multi=0, unbiased=False):
     """
     Function that takes experimental data and gives us the
     dependent/independent variables for analysis.
@@ -16,23 +16,47 @@ def read_colvar(filename):
     ----------
     filename : string
         Name of the COLVAR file to read in.
-
+    multi : int
+        Tells the method to read 1 or more COLVAR files. Default falsy
+        value (0) means read only 1 file.
+    unbiased : bool
+        If True, adds a 'weight' column of all 1s.
     Returns
     -------
     df : Pandas DataFrame
         CVs and bias as columns, time as index.
     """
     full_path = os.path.abspath(filename)
+    colvar_paths = [full_path]
 
-    with open(full_path, 'r') as f:
+    if multi:
+        colvar_paths = []
+        for i in range(0, multi):
+            replica_path = os.path.abspath(filename + "." + str(i))
+            colvar_paths.append(replica_path)
+
+    with open(colvar_paths[0], 'r') as f:
         header = f.readline().strip().split(" ")[2:]
 
-    df = pd.read_csv(filename, comment='#', names=header,
-                     delimiter='\s+', index_col=0)
+    frames = []
+    for path in colvar_paths:
+        df = pd.read_csv(path, comment='#', names=header,
+                         delimiter='\s+', index_col=0)
+
+        # provide default weight if simulation was unbiased
+        if unbiased:
+            df['weight'] = 1
+
+        if multi:
+            frames.append(df)
+
+    if multi:
+        df = pd.concat(frames, axis=0, join='outer', ignore_index=True)
+
     return df
 
 
-def read_hills(filename):
+def read_hills(filename='HILLS'):
     """
     Function that takes experimental data and gives us the
     dependent/independent variables for analysis.
--- plumitas/tests/test_plumitas.py
+++ plumitas/tests/test_plumitas.py
@@ -11,8 +11,8 @@ def test_read_files():
     Testing function to convert of COLVAR file to pandas DataFrame.
 
     """
-    colvar_file = op.join(data_path, "mini_colvar")
-    hills_file = op.join(data_path, "mini_hills")
+    colvar_file = op.join(data_path, "COLVAR")
+    hills_file = op.join(data_path, "HILLS")
 
     colvar_df = plm.read_colvar(colvar_file)
     hills_df = plm.read_hills(hills_file)
@@ -26,14 +26,21 @@ def test_make_2d_free_energy_surface():
     Testing function to convert of HILLS file to pandas DataFrame.
 
     """
-    colvar_file = op.join(data_path, "mini_colvar")
-    colvar_df = plm.read_colvar(colvar_file)
+    number_of_replicas = 4
 
-    axis = plm.make_2d_free_energy_surface(colvar_df,
-                                           colvar_df.columns[0],
-                                           colvar_df.columns[1],
+    colvar_name = op.join(data_path, "COLVAR")
+    single_colvar_df = plm.read_colvar(colvar_name, unbiased=True)
+    multi_colvar_df = plm.read_colvar(colvar_name,
+                                      multi=number_of_replicas,
+                                      unbiased=True)
+
+    axis = plm.make_2d_free_energy_surface(multi_colvar_df,
+                                           multi_colvar_df.columns[0],
+                                           multi_colvar_df.columns[1],
                                            temp=300)
 
+    assert (len(single_colvar_df.iloc[:, 0])
+            == len(multi_colvar_df.iloc[:, 0]) / number_of_replicas)
     assert axis
 
 
@@ -42,7 +49,7 @@ def test_potential_of_mean_force():
     Testing function to convert of HILLS file to pandas DataFrame.
 
     """
-    colvar_file = op.join(data_path, "mini_colvar")
+    colvar_file = op.join(data_path, "COLVAR")
     colvar_df = plm.read_colvar(colvar_file)
 
     axis = plm.potential_of_mean_force(colvar_df,
